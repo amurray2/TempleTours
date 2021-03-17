eb7162db0183c19c0dc4d648eff80d2df92d5537
Curtis <curtmparker@gmail.com>
2021-03-17 15:11:31 -0600

    Added Comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,27 +14,27 @@ namespace TempleTours.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        private TempleTourContext context { get; set; }
+        private TempleTourContext context { get; set; } //set up the context as an object of type TempleTourContext (which inherits from the DbContext and has two DbSets as properties)
 
         public HomeController(ILogger<HomeController> logger, TempleTourContext con)
         {
             _logger = logger;
-            context = con;
+            context = con; //sets the context
         }
 
-        public IActionResult Index()
+        public IActionResult Index() //returns the home page
         {
             return View();
         }
 
 
-        public IActionResult TimeSlots()//need to pass in all of the available time slots
+        public IActionResult TimeSlots()//passes in all of the available time slots
         {
             return View(context.Times.Where(t => t.Available == true));
         }
 
         [HttpGet]
-        public IActionResult SignUpForm(int timeId)
+        public IActionResult SignUpForm(int timeId) //Passes in the time that was selected
         {
             return View(new AppointmentFormViewModel { 
                 TimeSlot = context.Times.Single(t => t.TimeId == timeId)
@@ -42,14 +42,14 @@ namespace TempleTours.Controllers
         }
 
         [HttpPost]
-        public IActionResult SignUpForm(AppointmentFormViewModel a, int timeId)
+        public IActionResult SignUpForm(AppointmentFormViewModel a, int timeId) //Adds a new appointment to the database and marks the time slot used by that appointment as unavailable
         {
             if (ModelState.IsValid)
             {
                 context.Times.Single(t => t.TimeId == timeId).Available = false;
                 context.Appointments.Add(a.Appointment);
                 context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index"); //redirects to the home page
             }
             else
             {
@@ -60,7 +60,7 @@ namespace TempleTours.Controllers
             }
         }
 
-        public IActionResult Appointments()
+        public IActionResult Appointments() //passes in all of the appointment data.
         {
             return View(context.Appointments);
         }
